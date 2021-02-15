c7724ba1c6e49c5fe347a75126e8bf17edfdb860
Slooth <victor.guyot@epitech.eu>
2021-02-15 23:26:20 +0100

    almost 7

--- Day01/src/index.ts
+++ Day01/src/index.ts
@@ -1,3 +1,31 @@
 function helloWorld() {
   console.log('Hello World!');
 }
+
+import { Country, Mission, Cosmonaut } from './types';
+
+const axios = require('axios').default;
+
+function get_cosmonaut(tab : Array<any>, mission : Mission, country : Country) {
+  let array = [];
+  tab.forEach(function (element) {
+    if (element.mission === mission && element.country === country){
+      array.push(element);
+    }
+  });
+  return array;
+}
+
+async function selectCosmonaut(mission : Mission, country : Country) {
+  let array = [];
+  axios
+    .get('http://localhost:7600/cosmo/')
+    .then(function todo(resp) {
+      array = get_cosmonaut(resp.data, mission, country);
+      console.log(array);
+    })
+    .catch(function (error) {console.log(error)});
+  return (array);
+}
+let array = selectCosmonaut(Mission.ISS, Country.FR);
+console.log(array);
--- Day01/src/selectCosmonaut.ts
+++ Day01/src/selectCosmonaut.ts
@@ -1,6 +1,13 @@
 import { Country, Mission, Cosmonaut } from './types';
 
+const axios = require('axios').default;
+
 function selectCosmonaut(mission : Mission, country : Country)
 {
-
+    axios({
+        method: 'get',
+        url: 'http://localhost:7600/cosmo/'
+    }).then(function(resp){console.log(resp)})
+    .catch(function (error) {console.log(error)};
+})
 }
